63c72aeafd19d3cb2660c9309d7c374b0fa7cde6
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-11-01 20:13:57 +0100

    Movie1Detais

--- src/Pages/Movie1Details.js
+++ src/Pages/Movie1Details.js
@@ -1,5 +1,7 @@
 import useFetch from "../useFatch";
 import "../../src/App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 function Movie1Details() {
   const { data: movieFirst } = useFetch("https://api.tvmaze.com/shows/1");
@@ -10,7 +12,15 @@ function Movie1Details() {
       <p className="rating"> {movieFirst?.rating.average} 🌟🌟🌟🌟🌟🌟</p>
       <p className="genres"> {movieFirst?.genres}</p>
       <p className="pMovieOne"> {movieFirst?.summary}</p>
-      <p className="timeMovie">Time {movieFirst?.averageRuntime} min</p>
+      <div className="faIcons">
+        <FontAwesomeIcon icon={faClock} />
+        <p className="timeMovie"> {movieFirst?.averageRuntime} min </p>
+      </div>
+      <div className="premiered">
+        <div className="premiered">premiered: {movieFirst?.premiered}</div>
+        <div className="ended">ended: {movieFirst?.ended}</div>
+        <div className="schedule">schedule: {movieFirst?.schedule.time}</div>
+      </div>
     </div>
   );
 }
